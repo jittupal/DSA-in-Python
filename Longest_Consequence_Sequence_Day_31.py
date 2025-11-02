70200d99e9b74bb322f170b135402c7adcf9084c
jittupal <jksuperking3@gmail.com>
2025-11-02 18:50:32 +0530

    Day 31

--- Longest_Consequence_Sequence_Day_31.py
+++ Longest_Consequence_Sequence_Day_31.py
@@ -13,6 +13,25 @@ def longestConsecutive(arr):
         print(f"The Longest Sequence Is : {max_count}")
         #code here
 
+def longestConsecutive1(arr):
+        my_set = set()
+        n = len(arr)
+        for i in range(0, n):
+            my_set.add(arr[i])
+        
+        longest = 0
+        for num in my_set:
+            if num-1 not in my_set:
+                x = num
+                count = 1
+                while x+1 in my_set:
+                    count += 1
+                    x += 1
+                
+                longest = max(longest, count)
+                
+        print(f"The Longest Sequence Is : {longest}")
 
 arr = [1, 2,3, 78, 90, 4, 65, 2, 66, 67, 68, 69, 71, 72, 70]
 longestConsecutive(arr)
+longestConsecutive1(arr)
