c273dbe04f4d6ea5018ac6296347637d4e1abe9a
jitendra kumar <128616359+jittupal@users.noreply.github.com>
2025-11-22 20:40:30 +0530

    Day 46

--- Cycle_In_SLL_Day_46.py
+++ Cycle_In_SLL_Day_46.py
@@ -1,1 +1,50 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
+class SLL:
+    def __init__(self):
+        self.head = None
+
+    def append(self, val):
+        new_node = Node(val)
+        if self.head == None:
+            self.head = new_node
+        else:
+            curr = self.head
+            while curr.next is not None:
+                curr = curr.next
+            
+            curr.next = new_node
+    def traversal(self):
+        curr = self.head
+        while curr is not None:
+            print(curr.val, end="  ")
+            curr = curr.next
+    def detectLoop(self, head):
+        slow = head
+        fast = head
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                print("Linked List has Loop")
+        print("Linked List has no Loop")
+
+s = SLL()
+s.append(5)
+s.append(8)
+s.append(7)
+s.append(5)
+s.append(9)
+s.append(2)
+s.append(6)
+s.traversal()
+print()
+s.deleteion(3)
+s.traversal()
+print()
+s.deleteion(6)
+s.traversal()
+s.detectLoop()
