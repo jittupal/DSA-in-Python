facc2a4c511618603d64358b7f03e4d6d4bbc083
jittu-pal <jksuperking3@gmail.com>
2026-01-01 22:08:50 +0530

    Day 73

---
+++ getmin_stack_Day_73.py
@@ -0,0 +1,41 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        if len(self.stack) == 0:
+            self.stack.append([val, val])
+        else:
+            mini = min(self.stack[-1][1], val)
+            self.stack.append([val, mini])
+        
+
+    def pop(self) -> None:
+        if len(self.stack) == 0:
+            return
+        self.stack.pop()
+        
+
+    def top(self) -> int:
+        if len(self.stack) == 0:
+            return
+        print(self.stack[-1][0])
+        
+
+    def getMin(self) -> int:
+        if len(self.stack) == 0:
+            return
+        print(self.stack[-1][1])
+    
+s = MinStack()
+s.push(3)
+s.push(4)
+s.push(8)
+s.push(9)
+s.getMin()
+s.top()
+s.pop()
+s.pop()
+s.getMin()
