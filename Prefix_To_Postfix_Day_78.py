cc2b076cd168a9f73599bf07bb4cd4c4374bc2ab
jittu-pal <jksuperking3@gmail.com>
2026-01-17 21:08:23 +0530

    Day 78

---
+++ Prefix_To_Postfix_Day_78.py
@@ -0,0 +1,22 @@
+def PrefixToPostfix(s):
+    stack = []
+    s = s[::-1]
+    
+    for char in s:
+        if char.isalnum():
+            stack.append(char)
+        else:
+            operand2 = stack.pop()
+            operand1 = stack.pop()
+            
+            new_expression = operand2 + operand1 + char
+            
+            stack.append(new_expression)
+            
+    return stack[-1]
+
+s = "*+AB-CD"
+
+ans = PrefixToPostfix(s)
+
+print(ans)
