e62879c77c2ce190502596a28c8d1c187d5ea2b3
jittu-pal <jksuperking3@gmail.com>
2026-01-22 22:10:07 +0530

    Day 81

---
+++ Asteroids_Collision_Day_81.py
@@ -0,0 +1,23 @@
+class Solution:
+    def asteroidCollision(self, asteroids):
+        st = []
+        n = len(asteroids)
+        for i in range(0, n):
+            if asteroids[i] > 0:
+                st.append(asteroids[i])
+            else:
+                while len(st) != 0 and st[-1] > 0 and st[-1] < abs(asteroids[i]):
+                    st.pop()
+                if len(st) != 0 and st[-1] == abs(asteroids[i]):
+                    st.pop()
+                elif len(st) == 0 or st[-1] < 0:
+                    st.append(asteroids[i])
+        return st
+    
+    
+    
+arr = [2, 3, 5, 3, -4, 6]
+
+s = Solution()
+ans = s.asteroidCollision(arr)
+print(ans)
